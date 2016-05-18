229dacd52c2d99085fbd2ad2010f679b5a656c44
kobe <734369678@qq.com>
2016-05-18 16:27:25 +0800

    TestSet

---
+++ src/cn/bjsxt/collection/TestSet.java
@@ -0,0 +1,30 @@
+package cn.bjsxt.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 测试Set的常用方法
+ * @author ko
+ *
+ */
+public class TestSet {
+
+	public static void main(String[] args) {
+		Set set = new HashSet();
+		
+		set.add("aaa");
+		set.add("bbb");
+		set.add(new String("aaa"));
+		
+		System.out.println(set.size());
+		
+		System.out.println(set.contains("aaa"));
+		
+		set.remove("aaa");
+		
+		
+
+	}
+
+}
