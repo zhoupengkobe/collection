bcc3a321cbafbfa7efa00679107a1eba47e2aff4
kobe <734369678@qq.com>
2016-05-24 16:33:15 +0800

    119_自定义实现迭代器_深入迭代器_迭代器原理_面向对象实现

---
+++ src/cn/bjsxt/col/MyArrayList.java
@@ -0,0 +1,43 @@
+package cn.bjsxt.col;
+/**
+ * 简化迭代器
+ * hasNext
+ * next
+ * 
+ * @author ko
+ *
+ */
+public class MyArrayList {
+	private String[] elem = {"a","b","c","d","e","f","g"};
+	private int size = elem.length;
+	
+	private int cursor = -1;
+	/**
+	 * 判断是否存在下一个元素
+	 * @return
+	 */
+	public boolean hasNext() {
+		return cursor+1<size;
+	}
+	
+	/**
+	 * 获取下一个元素
+	 * @param args
+	 */
+	public String next(){
+		cursor++;//移动一次
+		return elem[cursor];
+	}
+	//删除元素
+	public void remove(){
+		
+	}
+	public static void main(String[] args) {
+		MyArrayList list = new MyArrayList();
+		while (list.hasNext()) {
+			System.out.println(list.next());
+		}
+
+	}
+
+}
