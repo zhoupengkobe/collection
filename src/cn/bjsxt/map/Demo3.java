e0b59569ccd88b04f729ed41004294d468379514
kobe <734369678@qq.com>
2016-05-24 17:52:54 +0800

    120_HashMap_经典存储_分拣思路_简单_容易

---
+++ src/cn/bjsxt/map/Demo3.java
@@ -0,0 +1,47 @@
+package cn.bjsxt.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * this is a cat and that is a mice and where is the food? 统计每个单词出现的次数
+ * 
+ * 存储到Map中 key :String value : 自定义类型
+ * 
+ * "分拣" 思路 1.为所有key创建容器 之后容器中存放对应的value 2.第一次创建容器，并存放值value 第二次之后，直接使用容器存放
+ * 
+ * @author ko
+ *
+ */
+public class Demo3 {
+
+	public static void main(String[] args) {
+		String str = "this is a cat and that is a mice and where is the food";
+		// 分割字符串
+		String[] strArray = str.split(" ");
+		// 存储到Map中
+		Map<String, Letter> letters = new HashMap<String, Letter>();
+
+		for (String temp : strArray) {
+			// 为所有的key创建容器
+			if (!letters.containsKey(temp)) {
+				Letter col = new Letter();
+				col.setCount(1);// 第一次值存放容器
+				letters.put(temp, col);
+			}
+			// 之后容器中存放对应的value
+			Letter col = letters.get(temp);// 直接使用容器
+			col.setCount(col.getCount() + 1);
+		}
+
+		// 输出Map的值
+		Set<String> keys = letters.keySet();
+		for (String key : keys) {
+			Letter col = letters.get(key);
+			System.out.println("字母" + key + ",次数" + col.getCount());
+		}
+
+	}
+
+}
